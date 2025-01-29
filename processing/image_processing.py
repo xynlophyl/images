e5f9db93c43675408dff412e49f2feee47f306a6
Nelson Lin <62612858+xynlophyl@users.noreply.github.com>
2025-01-28 23:56:08 -0500

    Modify gaussian blur parameters
    Added mask_size as a parameter for gaussian_blur function
    to allow for more flexible functionality

--- processing/image_processing.py
+++ processing/image_processing.py
@@ -31,10 +31,9 @@ class ImageProcessor():
     def _gaussian(self, x: int, y: int, std_dev: float) -> np.ndarray:
         return (1/(2*np.pi*std_dev**2))*np.exp(-(x**2+y**2)/(2*std_dev**2))
 
-    def gaussian_blur(self, image: np.ndarray, std_dev: float = 1.0, stride: int = 1, mode: str = "convolve") -> np.ndarray:
-        kernel_size = int(std_dev*3)     
-        center_x = center_y = kernel_size//2
-        gaussian_mask = np.array([[self._gaussian(x-center_x, y-center_y, std_dev) for x in range(kernel_size)] for y in range(kernel_size)])
+    def gaussian_blur(self, image: np.ndarray, std_dev: float = 1.0, mask_size: int = 3, stride: int = 1, mode: str = "convolve") -> np.ndarray:
+        center_x = center_y = mask_size//2
+        gaussian_mask = np.array([[self._gaussian(x-center_x, y-center_y, std_dev) for x in range(mask_size)] for y in range(mask_size)])
         mask_rgb = np.repeat(np.expand_dims(gaussian_mask, axis = -1), 3, axis = -1)
 
         return self._apply_mask(image, mask_rgb, stride = stride, mode = mode)

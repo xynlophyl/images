406503ac0c543ae6077e080d90fe37e950743d0f
Nelson Lin <62612858+xynlophyl@users.noreply.github.com>
2025-02-03 22:01:05 -0500

    Refactor twiddle generation to improve runtime
    Refactored the calculation of twiddle factors
    at each fft and inv_fft recursion step
    utilizing memoization in order to speed up duplicate calls.

--- processing/fourier_transform.py
+++ processing/fourier_transform.py
@@ -1,9 +1,14 @@
 import numpy as np
+import functools
     
 def get_next_power_of_two(x):
     exponent = np.log2(x)
 
     return np.ceil(exponent).astype(int)
+
+@functools.cache
+def generate_twiddle(n):
+    return np.exp(2*np.pi*1j*np.arange(n//2)/n)
 
 def fast_fourier_transform(mat: np.ndarray):
     def _fft_recur(polynomial: np.ndarray):
@@ -15,9 +20,15 @@ def fast_fourier_transform(mat: np.ndarray):
         even_fft, odd_fft = _fft_recur(evens), _fft_recur(odds)
 
         # compute twiddle factors
-        twiddle = np.exp(2*np.pi*1j*np.arange(n//2)/n)*odd_fft
+        twiddle = generate_twiddle(n)*odd_fft
 
-        return np.concat([even_fft+twiddle, even_fft-twiddle])
+        # generate output of current recursion
+        output = np.zeros((n,), dtype = twiddle.dtype)
+
+        output[:n//2] = even_fft+twiddle
+        output[n//2:] = even_fft-twiddle
+
+        return output
     
     polynomial = mat.reshape(-1,)
     N = len(polynomial)
@@ -38,12 +49,24 @@ def inverse_fast_fourier_transform(mat: np.ndarray):
         even_fft, odd_fft = _inverse_fft_recur(evens), _inverse_fft_recur(odds)
 
         # compute twiddle factors
-        twiddle_inv = np.exp(-2*np.pi*1j*np.arange(n//2)/n)*odd_fft
+        twiddle_inv = (1/generate_twiddle(n))*odd_fft
+        
+        # generate output of current recursion
+        output = np.zeros((n,), dtype = twiddle_inv.dtype)
 
-        return np.concat([even_fft+twiddle_inv, even_fft-twiddle_inv])
+        output[:n//2] = even_fft+twiddle_inv
+        output[n//2:] = even_fft-twiddle_inv
+
+        return output
     
     polynomial = mat.reshape(-1,)
     n = len(polynomial)
     output = _inverse_fft_recur(polynomial) / n
 
     return np.round(output)
+
+"""
+TODO: 
+1. FIGURE OUT HOW TO REMOVE PADDING FOR FINAL OUTPUT IMAGE
+2. OPTIMIZE PERFORMANCE (creating the output matrix takes too long currently, figure out a way of allocating the space before running through the recursion?)
+"""
